61bd262277a1ab61d342d65fb7c551131ddf3d9e
ABHISHEK BADIGER <86523668+codeAbhi2000@users.noreply.github.com>
2023-03-01 15:28:12 +0530

    done with basic structure

--- src/App.js
+++ src/App.js
@@ -1,11 +1,30 @@
 
 import './App.css';
+import { BrowserRouter as Router ,Routes,Route,Link } from 'react-router-dom';
+import Home from './pages/Home'
+import EditorDash from './pages/EditorDash';
+import Login from './pages/Login';
 
 function App() {
   return (
-    <div className="App">
-     
-    </div>
+    <Router>
+      <nav>
+        <div className="logoTle">
+            <div className="logo"></div>
+            <span>Vviet-Media</span>
+        </div>
+        <ul>
+            <Link to='/'>Home</Link>
+            <Link to='/login'>Login</Link>
+            <Link to='/editorDash'>EditorDash</Link>
+        </ul>
+      </nav>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/login' element={<Login/>}/>
+        <Route path='/editorDash' element={<EditorDash/>}/>
+      </Routes>
+    </Router>
   );
 }
 
---
+++ src/pages/EditorDash.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function EditorDash() {
+  return (
+    <div>EditorDash</div>
+  )
+}
+
+export default EditorDash
